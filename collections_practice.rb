46c93f8593c1db3d1c1e0bc82f567e5fad7cd1d6
Oliver <oliver.grant@gmail.com>
2019-03-12 05:39:37 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc (int_list)
+  int_list.sort
+end
 
+def sort_array_desc (int_list)
+  int_list.sort_by{ |a| a }.reverse
+end
+
+def sort_array_char_count (str_list)
+  str_list.sort_by {|a| a.length}
+end
+
+def swap_elements (ary)
+  ary.insert(1,ary.delete_at(2))
+  ary
+end
+
+def reverse_array (ary)
+  ary.reverse!
+end
+
+def kesha_maker (ary)
+  ary.map {|a| a[2] = "$"}
+  ary
+end
+
+def find_a (ary)
+  ary.select {|a| a[0] = "a" && != "arange" && != "aear" }
+end
